73f4e5c687e7063e5c5a7d2184fb5c15984db27a
Moamen Mostafa <moemen.a1@gmail.com>
2023-09-07 09:47:46 +0200

    init

---
+++ code.py
@@ -0,0 +1,88 @@
+import board
+import digitalio
+import rotaryio
+import time
+import usb_hid
+import keypad
+from hid_gamepad import Gamepad
+
+# Set up the HID Gamepad
+gp = Gamepad(usb_hid.devices)
+
+# KeyMatrix setup
+km = keypad.KeyMatrix(
+    row_pins=(board.GP2, board.GP1),
+    column_pins=(board.GP9, board.GP8, board.GP4, board.GP6, board.GP7, board.GP0, board.GP5),
+)
+
+
+# Encoders setup
+encoder_pins = [(board.GP10, board.GP11), (board.GP13, board.GP14), (board.GP16, board.GP17), (board.GP19, board.GP20)]
+encoders = [rotaryio.IncrementalEncoder(pinA, pinB) for pinB, pinA in encoder_pins]
+
+encoder_buttons_pins = [board.GP12, board.GP15, board.GP18, board.GP22]
+encoder_buttons = [digitalio.DigitalInOut(pin) for pin in encoder_buttons_pins]
+for encoder_button in encoder_buttons:
+    encoder_button.direction = digitalio.Direction.INPUT
+    encoder_button.pull = digitalio.Pull.UP
+
+# Define the keycodes for the buttons
+button_keycodes = [
+    1, 2, 3, 4, 5, 6, 7,
+    8, 9, 10, 11, 12, 13, 14
+]
+
+# Define the keycodes for the encoders
+encoder_keycodes = [
+    [15, 16, 17],
+    [18, 19, 20],
+    [21, 22, 23],
+    [24, 25, 26]
+]
+
+# Initialize encoder buttons state list with False value
+encoder_button_state = [False for i in range(len(encoder_buttons))]
+
+# Keypress and release
+def keyPress(key):
+    gp.press_buttons(key)
+
+def keyRelease(key):
+    gp.release_buttons(key)
+
+# Main loop
+while True:
+    # KeyMatrix reading and handling
+    event = km.events.get()
+    if event:
+        if event.pressed:
+            print(event)
+            keyPress(button_keycodes[event.key_number])
+        if event.released:
+            print(event)
+            keyRelease(button_keycodes[event.key_number])
+
+
+
+    # Encoders reading and handling
+    for idx, encoder in enumerate(encoders):
+        if encoder.position != 0:
+            print("Encoder {}: Position = {}".format(idx, encoder.position))
+            print("encoder_keycode = {}".format(encoder_keycodes[idx][encoder.position + 1]))
+            keyPress(encoder_keycodes[idx][encoder.position + 1])
+            keyRelease(encoder_keycodes[idx][encoder.position + 1])
+            encoder.position = 0
+
+    # Encoder Buttons reading and handling
+    for idx, encoder_button in enumerate(encoder_buttons):
+        if not encoder_button.value and not encoder_button_state[idx]:
+            print("encoder_buttons {} Pressed".format(idx))
+            keyPress(encoder_keycodes[idx][1])
+            encoder_button_state[idx] = True
+        if encoder_button.value and encoder_button_state[idx]:
+            print("encoder_buttons {} Released".format(idx))
+            keyRelease(encoder_keycodes[idx][1])
+            encoder_button_state[idx] = False
+
+
+    time.sleep(0.01)  # Add a small delay to avoid flooding the USB connection
---
+++ hid_gamepad.py
@@ -0,0 +1,107 @@
+# SPDX-FileCopyrightText: 2018 Dan Halbert for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`Gamepad`
+====================================================
+
+* Author(s): Dan Halbert
+"""
+
+import struct
+import time
+
+from adafruit_hid import find_device
+
+
+class Gamepad:
+    """Emulate a generic gamepad controller with 16 buttons,
+    numbered 1-32.
+
+    The joystick values could be interpreted
+    differently by the receiving program: those are just the names used here.
+    The joystick values are in the range -127 to 127."""
+
+    def __init__(self, devices):
+        """Create a Gamepad object that will send USB gamepad HID reports.
+
+        Devices can be a list of devices that includes a gamepad device or a gamepad device
+        itself. A device is any object that implements ``send_report()``, ``usage_page`` and
+        ``usage``.
+        """
+        self._gamepad_device = find_device(devices, usage_page=0x1, usage=0x05)
+
+        # Reuse this bytearray to send mouse reports.
+        # Typically controllers start numbering buttons at 1 rather than 0.
+        # report[0] buttons 1-8 (LSB is button 1)
+        # report[1] buttons 9-16
+        # report[2] buttons 17-24
+        # report[3] buttons 25-32
+        self._report = bytearray(4)
+
+        # Remember the last report as well, so we can avoid sending
+        # duplicate reports.
+        self._last_report = bytearray(4)
+
+        # Store settings separately before putting into report. Saves code
+        # especially for buttons.
+        self._buttons_state = 0
+
+        # Send an initial report to test if HID device is ready.
+        # If not, wait a bit and try once more.
+        try:
+            self.reset_all()
+        except OSError:
+            time.sleep(1)
+            self.reset_all()
+
+    def press_buttons(self, *buttons):
+        """Press and hold the given buttons."""
+        for button in buttons:
+            self._buttons_state |= 1 << self._validate_button_number(button) - 1
+        self._send()
+
+    def release_buttons(self, *buttons):
+        """Release the given buttons."""
+        for button in buttons:
+            self._buttons_state &= ~(1 << self._validate_button_number(button) - 1)
+        self._send()
+
+    def release_all_buttons(self):
+        """Release all the buttons."""
+
+        self._buttons_state = 0
+        self._send()
+
+    def click_buttons(self, *buttons):
+        """Press and release the given buttons."""
+        self.press_buttons(*buttons)
+        self.release_buttons(*buttons)
+
+    def reset_all(self):
+        """Release all buttons and set joysticks to zero."""
+        self._buttons_state = 0
+        self._send(always=True)
+
+    def _send(self, always=False):
+        """Send a report with all the existing settings.
+        If ``always`` is ``False`` (the default), send only if there have been changes.
+        """
+        struct.pack_into(
+            "<I",
+            self._report,
+            0,
+            self._buttons_state,
+        )
+
+        if always or self._last_report != self._report:
+            self._gamepad_device.send_report(self._report)
+            # Remember what we sent, without allocating new storage.
+            self._last_report[:] = self._report
+
+    @staticmethod
+    def _validate_button_number(button):
+        if not 1 <= button <= 32:
+            raise ValueError("Button number must in range 1 to 32")
+        return button
