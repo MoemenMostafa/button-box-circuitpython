7cc2947d3ef70de71d84ad74c706bf982ece5bcc
Moamen Mostafa <moemen.a1@gmail.com>
2024-05-19 13:23:45 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -12,13 +12,13 @@ GAMEPAD_REPORT_DESCRIPTOR = bytes((
     0x85, 0x04,  #   Report ID (4)
     0x05, 0x09,  #   Usage Page (Button)
     0x19, 0x01,  #   Usage Minimum (Button 1)
-    0x29, 0x20,  #   Usage Maximum (Button 32)
+    0x29, 0x10,  #   Usage Maximum (Button 16)
     0x15, 0x00,  #   Logical Minimum (0)
     0x25, 0x01,  #   Logical Maximum (1)
     0x75, 0x01,  #   Report Size (1)
-    0x95, 0x20,  #   Report Count (32)
+    0x95, 0x10,  #   Report Count (16)  ## 2 bytes
     0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,        # End Collection
+    0xC0,        # End Collection      ## total 2 bytes
 ))
 
 gamepad = usb_hid.Device(
@@ -26,7 +26,7 @@ gamepad = usb_hid.Device(
     usage_page=0x01,           # Generic Desktop Control
     usage=0x05,                # Gamepad
     report_ids=(4,),           # Descriptor uses report ID 4.
-    in_report_lengths=(4,),    # This gamepad sends 6 bytes in its report.
+    in_report_lengths=(2,),    # This gamepad sends 6 bytes in its report.
     out_report_lengths=(0,),   # It does not receive any reports.
 )
 
@@ -36,7 +36,7 @@ usb_hid.enable(
 )
 
 # adding a way out of the serial and usb disable (by pressing the button connected to GP21 usb drive and serial will be enabled)
-button = digitalio.DigitalInOut(board.GP21)
+button = digitalio.DigitalInOut(board.GP9)
 button.pull = digitalio.Pull.UP
 
 if button.value:
